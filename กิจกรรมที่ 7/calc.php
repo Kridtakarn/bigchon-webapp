9e7cf48c5b4c7f827dc5f7bdd6ba1c9b09758d77
unknown <knantajan@gmail.com>
2020-08-23 18:03:54 +0700

    ปรับเว้นวรรค

--- กิจกรรมที่ 7/calc.php
+++ กิจกรรมที่ 7/calc.php
@@ -3,8 +3,8 @@
     $args2 = $_POST["args2"];
     $operations = $_POST["operations"];
 
-    if ($operations == 1) echo "Result of triangle that there are base $args1 unit and high $args2 unit is", $args1*$args2/2, "unit^2";
-    else if ($operations == 2) echo "Result of rectangle that there are wide $args1 unit and long $args2 unit is", $args1*$args2 ,"unit^2";
+    if ($operations == 1) echo "Result of triangle that there are base $args1 unit and high $args2 unit is ", $args1*$args2/2, " unit^2";
+    else if ($operations == 2) echo "Result of rectangle that there are wide $args1 unit and long $args2 unit is ", $args1*$args2 ," unit^2";
     else if ($operations == 3) echo "$args1 + $args2 = ", $args1+$args2;
     else if ($operations == 4) echo "$args1 - $args2 = ",$args1-$args2;
     else if ($operations == 5) echo "$args1 * $args2 = ",$args1*$args2;
